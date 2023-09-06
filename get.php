9afc034b8c6236d6a4490c9ae16aa66c9db94645
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-06 14:08:21 -0500

    Update get.php

--- get.php
+++ get.php
@@ -1,1 +1,12 @@
-
+<?php
+$PageTitle = "Get";
+include "view-header.php";
+?>
+    <h1>Post</h1>
+<form method = "get" action = "get-result.php>
+  <input type="text" name = 'my-name'>
+  <input type ="submit" value = "Get Data">
+  </form>
+<?php
+include "view-footer.php";
+?>
