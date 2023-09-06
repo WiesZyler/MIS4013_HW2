c0238763c9ee7ecad2034c06d88a5eaaf2ad14ee
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-06 14:34:41 -0500

    Update get.php

--- get.php
+++ get.php
@@ -5,7 +5,7 @@ include "view-header.php";
     <h1>Post</h1>
 <form method = "get" action = "get-result.php">
   <input type="text" name = 'my-name'>
-  <input type ="submit" value = "Get Data">
+  <input type ="submit" value = "Get Data" style="color: black;">
   </form>
 <?php
 include "view-footer.php";
