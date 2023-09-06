ebed8d927b3dce6360d05bff34e80784913587b7
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-06 14:15:50 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -5,24 +5,20 @@ include "view-header.php";
     <h1>Post Result</h1>
 
 <?php
+echo getDisplay();
+include "view-footer.php";
+
+function getDisplay()
+{
 if (isset($_POST['my-name']))
 {
-?>
-<p>The Value Sent is: </p>
-<?php
-  echo $_POST['mmy-name'];
+return "<p>The Value Sent is: </p>" . $_POST['mmy-name'];
 }
 else
 {
-?>
-
-<p>Nothing Posted to the page!</p>
+return "<p>Nothing Posted to the page!</p>";
 <?php
-
-  
 }
 
-
-
-include "view-footer.php";
+}
 ?>
